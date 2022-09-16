c4b825c8c905cbf039f9f34ecb26734aa9e58a6f
JovanPetkovic99 <jovanpetkovicdevelopment@gmail.com>
2022-09-16 23:28:08 +0200

    Added tailwind

---
+++ resources/views/components/item.blade.php
@@ -0,0 +1,1 @@
+<div class="w-80 h-80 bg-amber-900 m-10"></div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex flex-row w-full justify-around flex-wrap">
+        <x-item></x-item>
+        <x-item></x-item>
+        <x-item></x-item>
+        <x-item></x-item>
+        <x-item></x-item>
+        <x-item></x-item>
+    </div>
+</body>
+</html>
+
+
